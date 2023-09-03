28bb7e56286cd7b5f1650f6834a589a1400fcbe3
SANTAZ10 <fagbolasamuel.sf@gmail.com>
2023-09-03 10:27:26 +0100

    last one

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return(
-    <div className=" w-full absolute bottom-0 mt-10 p-4">
+    <div className=" w-full absolute bottom-0 mt-28 p-4">
       <div className="flex items-center justify-center">
         <h1 className="">
           Made by <span className="text-[#068FFF]">Santaz</span> 
